cd58d36958062ff35e0b7efc057f6507cab010d6
ValentinLeisterPersonal <valentin.leister@gmail.com>
2021-05-21 15:33:20 +0200

    refactoring the daily scrape

--- daily_scrape_and_write.py
+++ daily_scrape_and_write.py
@@ -16,6 +16,9 @@ from tqdm import tqdm
 import psycopg2
 import psycopg2.extras as extras
 
+from scrapers import FishPrices 
+
+
 def execute_values(conn, df, table):
     """
     Using psycopg2.extras.execute_values() to insert the dataframe
@@ -48,83 +51,22 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 #os.chdir(r'C:\Users\valen\desktop\fish_ax')
 
 
-# scraping Website HTML
-print("Starting scraping fish prices from Vigo...")
-page = requests.get('http://www.puertodevigo.com/informacion-diaria-lonja/')
-soup = BeautifulSoup(page.text, "html.parser")
-print("Done scraping")
+###### Running the scraping of the website
 
-# Find table call "listado" which contains fish auction prices
-table = soup.findChildren(name="table", attrs={"class":"listado"})[0]
+fp = FishPrices(url= 'http://www.puertodevigo.com/informacion-diaria-lonja/', conn= conn)
 
-# Extract each row of the table but nothing else
-rows = table.findChildren(['td', 'tr'])
+fp.extract_rows()
 
+fp.create_df()
 
-#creating table to write content into
-columns = ["species","max_price", "min_price", "kg_auctioned"]
-df_today = pd.DataFrame(columns = columns)
+fp.rows_to_df()
 
-#write each row to table
-for row in tqdm(rows):
-    i=0
-    cells = row.findChildren('td')
-    if len(cells) >0:
-        #print(cells)
-        i=0
-        row_entries = list()
-        for cell in cells:
-            i+=1
-            value = cell.string
-            # if it´s the first cell in the row, then we´re looking at the string and do not need to do any number extraction
-            if i ==1:
-                row_entries.append(value)
-                
-            # all other cells need to be cleaned of currency format and other text
-            else:
-                row_entries.append(re.findall(r'(^(?!0+\,00)(?=.{1,9}(\,|$))(?!0(?!\,))\d{1,3}(.\d{3})*(\,\d+)?|$)', value)[0][0])
-                
-        # We add the extracted data at the bottom of the data frame
-        df_length = len(df_today)
-        df_today.loc[df_length] = row_entries
-        
-# convert all strings that have been scraped and represent numeric values into floats
-df_today_clean = df_today.apply(lambda x: x.str.replace('[A-Za-z]', '').str.replace('.', '').str.replace(',', '.').astype(float) if x.name in ['max_price','min_price', 'kg_auctioned'] else x)
+fp.convert_strings_to_float()
 
-# Add a date column with today´s date. 
-df_today_clean.insert(1, 'date', datetime.date.today())
-print('Data for today looks like this:')
-print(df_today_clean.head())
+fp.add_todays_date()
 
-# Load all existing data to see whether we already have some entries for today
+fp.load_historic_data(sql = 'select * from market_price_vigo_hist_daily')
 
-sql = 'select * from market_price_vigo_hist_daily'
-df_hist = pd.read_sql_query(sql,conn)
+fp.check_if_new_data_available()
 
-
-if len(df_today_clean)>len(df_hist[datetime.date.today() == df_hist.date]):
-    print('We found new data from today. Removing any previous records previous to appending...')
-
-    # Dropping rows where date is not today
-    #df_hist = df_hist[datetime.date.today() != df_hist.date]
-    cur = conn.cursor()
-    cur.execute("""delete from market_price_vigo_hist_daily where date = %s""",(datetime.date.today(),))
-    print('Step 1: Successfully dropped previously pulled record for today')
-
-    print('Records stored before appending:'+str(len(pd.read_sql_query(sql,conn))))
-    # Appending new rows to pg table
-    execute_values(conn, df_today_clean, 'market_price_vigo_hist_daily')
-    print(str(len(pd.read_sql_query(sql,conn))))
-
-    print(str(len(df_today_clean))+' new rows successfully apended')
-    print('Records stored after appending:'+str(len(pd.read_sql_query(sql,conn))))
-else:
-    print('No new records for today. Not making any updates to the database')
-
-# Updating the main database
-#df_updated.to_csv('data/market_price_vigo_hist_daily.csv',index=False, header =True)
-#print('Data successfully written to .csv')
-
-# Writing a backup with a timestamp, just in case
-#df_updated.to_csv('data/backup/market_price_vigo_hist_daily_backup'+datetime.datetime.now().strftime("%d-%b-%Y-%H-%M-%S")+'.csv',index=False, header =True)
-#print('Data Backup successfully written')
+fp.update_db()
---
+++ scrapers.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 29 17:24:58 2021
+
+@author: valen
+"""
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import datetime
+import re
+import os
+from tqdm import tqdm
+import psycopg2
+import psycopg2.extras as extras
+
+class FishPrices():
+    def __init__(self, url, conn):
+        self.conn = conn
+        # scraping Website HTML
+        print("Starting scraping fish prices from Vigo...")
+        page = requests.get(url)
+        self.soup = BeautifulSoup(page.text, "html.parser")
+        print("Done scraping")
+    
+    def extract_rows(self):
+        # Find table call "listado" which contains fish auction prices
+        table = self.soup.findChildren(name="table", attrs={"class":"listado"})[0]
+        
+        # Extract each row of the table but nothing else
+        self.rows = table.findChildren(['td', 'tr'])
+        
+    def create_df(self):
+        #creating table to write content into
+        columns = ["species","max_price", "min_price", "kg_auctioned"]
+        self.df = pd.DataFrame(columns = columns)
+        
+    
+    def rows_to_df(self):
+        
+        #write each row to table
+        for row in tqdm(self.rows):
+            i=0
+            cells = row.findChildren('td')
+            if len(cells) >0:
+                #print(cells)
+                i=0
+                row_entries = list()
+                for cell in cells:
+                    i+=1
+                    value = cell.string
+                    # if it´s the first cell in the row, then we´re looking at the string and do not need to do any number extraction
+                    if i ==1:
+                        row_entries.append(value)
+                        
+                    # all other cells need to be cleaned of currency format and other text
+                    else:
+                        row_entries.append(re.findall(r'(^(?!0+\,00)(?=.{1,9}(\,|$))(?!0(?!\,))\d{1,3}(.\d{3})*(\,\d+)?|$)', value)[0][0])
+                        # We add the extracted data at the bottom of the data frame
+                df_length = len(self.df)
+                self.df.loc[df_length] = row_entries
+        
+    def convert_strings_to_float(self):
+        # convert all strings that have been scraped and represent numeric values into floats
+        self.df_clean = self.df.apply(lambda x: x.str.replace('[A-Za-z]', '').str.replace('.', '').str.replace(',', '.').astype(float) if x.name in ['max_price','min_price', 'kg_auctioned'] else x)
+
+
+    def add_todays_date(self):
+        # Add a date column with today´s date. 
+        self.df_clean.insert(1, 'date', datetime.date.today())
+        print('Data for today looks like this:')
+        print(self.df_clean.head())
+    
+    
+    def load_historic_data(self, sql):
+        # Load all existing data to see whether we already have some entries for today
+        self.sql = sql
+        self.df_hist = pd.read_sql_query(sql, self.conn)
+        
+        
+    def check_if_new_data_available(self):
+        if len(self.df_clean)>len(self.df_hist[datetime.date.today() == self.df_hist.date]):
+            self.isfreshdata = True
+        else:
+            self.isfreshdata = False
+
+    
+    def update_db(self):
+        if self.isfreshdata == True:
+            print('We found new data from today.')
+            self.drop_stale_data()
+            self.append_fresh_data()
+        else:
+            print('No new records for today. Not making any updates to the database')
+            
+          
+    def drop_stale_data(self):
+        # Dropping rows where date is not today
+        print('Removing any previous records previous to appending...')
+        cur = self.conn.cursor()
+        cur.execute("""delete from market_price_vigo_hist_daily where date = %s""",(datetime.date.today(),))
+        print('Step 1: Successfully dropped previously pulled record for today')
+        
+    
+    def append_fresh_data(self):
+        print('Records stored before appending:'+str(len(pd.read_sql_query(self.sql,self.conn))))
+        
+        # Appending new rows to pg table
+        execute_values(self.conn, self.df_clean, 'market_price_vigo_hist_daily')
+        print(str(len(pd.read_sql_query(self.sql,self.conn))))
+    
+        print(str(len(self.df_clean))+' new rows successfully apended')
+        print('Records stored after appending:'+str(len(pd.read_sql_query(self.sql,self.conn))))
+
